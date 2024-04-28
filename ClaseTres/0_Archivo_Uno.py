ef6af4f989bd3329bb86495f9bd78474f5719da7
Karolina271 <167647802+Karolina271@users.noreply.github.com>
2024-04-27 19:02:04 -0500

    clase tres
    se creo un archivo donde miramos los tipos de variables y listas

---
+++ ClaseTres/0_Archivo_Uno.py
@@ -0,0 +1,14 @@
+#esta es una variable 
+variable = 8
+variable_dos = "Texto dos"
+my_string = "thisStringisAwesome"
+
+my_string.upper()
+print(my_string)
+print(variable_dos)
+#print () permite mostrar en la terminal la información
+
+print("Hola")
+print(type(8))
+print(type(1.8))
+print(type("texto"))
